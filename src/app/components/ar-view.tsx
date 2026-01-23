c26423390a7bbe23f673ce8428e78a10b88bb81f
Graham Lyons <glyons@beyond.com>
2026-01-23 09:26:46 +0000

    Added gitignore and typing for three js

--- src/app/components/ar-view.tsx
+++ src/app/components/ar-view.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import Box from '@/app/components/box';
 
 interface ARViewProps {
