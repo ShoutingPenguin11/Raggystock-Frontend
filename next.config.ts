d5de4a01e3b33f337481f69ad5956de3440392ab
Graham Lyons <glyons@beyond.com>
2026-02-03 16:35:38 +0000

    Forcing webpack instead of turbo pack for build and dev

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
+// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 
   images: {
